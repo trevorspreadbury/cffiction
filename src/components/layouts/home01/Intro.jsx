f7ac729ad71df9cd8b8b84415ec52f53bd02d6fc
Trevor Spreadbury <trevorspreadbury@gmail.com>
2022-09-13 21:08:54 -0500

    add books list and reorder components

--- src/components/layouts/home01/Intro.jsx
+++ src/components/layouts/home01/Intro.jsx
@@ -4,37 +4,31 @@ import {Link} from 'react-router-dom'
 class Intro extends Component {
     render() {
         return (
-            <section className="pt-100 center-about bg-grey">
-                <div className="container">
-                    <div className="row justify-content-center">
-                        <div className="col-lg-8">
-                            <div className="section-heading text-center">
-                                <Link to="https://www.amazon.com/Name-Book-Collection-Interesting-Self-Serving/dp/1717867936">Click here to buy The Name of the Book now on Amazon! </Link>
-                            </div>
-                            <div className="section-heading text-center">
-                                <Link to="https://www.amazon.com/Calamitous-Fortunes-Fiction-Christopher-Spreadbury/dp/107645108X">Click here to buy Calamitous Fortunes now on Amazon! </Link>
-                            </div>
-                        </div>
-                    </div>
-                </div>
+            <section id="shop" className="pt-100 center-about bg-grey">
 
                 <section className="cta-2 clients ">
                     <div className="container">
                         <div className="row">
-                            <div className="col-lg-4 col-sm-8 ">
-                                <div className="client-logo">
-                                    <img src="assets/images/saskiasNever.jpg" alt="" className="img-fluid"/>
-                                </div>
+                            <div className="col-lg-4 col-sm-4 ">
+                                <a href="https://www.amazon.com/Name-Book-Collection-Interesting-Self-Serving/dp/1717867936">
+                                    <div className="client-logo">
+                                        <img src="assets/images/theNameOfTheBook.jpg" alt="" className="img-fluid"/>
+                                    </div>
+                                </a>
                             </div>
-                            <div className="col-lg-4 col-sm-8">
-                                <div className="client-logo">
-                                    <img src="assets/images/calamitousFortunes.jpg" alt="" className="img-fluid"/>
-                                </div>
+                            <div className="col-lg-4 col-sm-4">
+                                <a href="https://www.amazon.com/Calamitous-Fortunes-Fiction-Christopher-Spreadbury/dp/107645108X">
+                                    <div className="client-logo">
+                                        <img src="assets/images/calamitousFortunes.jpg" alt="" className="img-fluid"/>
+                                    </div>
+                                </a>
                             </div>
-                            <div className="col-lg-4 col-sm-8">
-                                <div className="client-logo">
-                                    <img src="assets/images/theNameOfTheBook.jpg" alt="" className="img-fluid"/>
-                                </div>
+                            <div className="col-lg-4 col-sm-4">
+                                <a href="https://www.amazon.com/Saskias-Never-Repairmans-Never-Ending-Story/dp/B09WPZ9PQR/">
+                                    <div className="client-logo">
+                                        <img src="assets/images/saskiasNever.jpg" alt="" className="img-fluid"/>
+                                    </div>
+                                </a>
                             </div>
                         </div>
                     </div>
--- src/components/pages/Home01.jsx
+++ src/components/pages/Home01.jsx
@@ -1,16 +1,15 @@
 import React, { Component } from 'react';
-import {Header, Banner, Intro, Chapter, About, Features, Feature2, Counter,
-    Cta, Review, Author, Pricing, Faq,Contact,Footer} from '../layouts/home01/index';
+import {Header, Banner, Intro, About, Review, Contact, Footer, Books } from '../layouts/home01/index';
 
 class Home01 extends Component {
     render() {
         return (
             <div className="counter-scroll bg-home1 has-one-page">       
                 <Header />       
-                        <Banner />
                         <Intro />
-                        <About />
+                        <Books />
                         <Review />
+                        <About />
                         <Contact />
                 <Footer />
             </div>

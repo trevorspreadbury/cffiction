46ffec6c166bbb5104ce167c04e026c471cb65a0
Trevor Spreadbury <trevorspreadbury@gmail.com>
2022-10-12 21:54:58 -0500

    keep images in a row on small screens

--- src/components/layouts/home01/Intro.jsx
+++ src/components/layouts/home01/Intro.jsx
@@ -17,21 +17,21 @@ class Intro extends Component {
                 <section className="cta-2 clients ">
                     <div className="container">
                         <div className="row">
-                            <div className="col-lg-4 col-sm-4 ">
+                            <div className="col-lg-4 col-4 col-sm-4">
                                 <a href="https://www.amazon.com/Name-Book-Collection-Interesting-Self-Serving/dp/1717867936">
                                     <div className="client-logo">
                                         <img src="assets/images/theNameOfTheBook.jpg" alt="" className="img-fluid"/>
                                     </div>
                                 </a>
                             </div>
-                            <div className="col-lg-4 col-sm-4">
+                            <div className="col-lg-4 col-4 col-sm-4">
                                 <a href="https://www.amazon.com/Calamitous-Fortunes-Fiction-Christopher-Spreadbury/dp/107645108X">
                                     <div className="client-logo">
                                         <img src="assets/images/calamitousFortunes.jpg" alt="" className="img-fluid"/>
                                     </div>
                                 </a>
                             </div>
-                            <div className="col-lg-4 col-sm-4">
+                            <div className="col-lg-4 col-4 col-sm-4">
                                 <a href="https://www.amazon.com/Saskias-Never-Repairmans-Never-Ending-Story/dp/B09WPZ9PQR/">
                                     <div className="client-logo">
                                         <img src="assets/images/saskiasNever.jpg" alt="" className="img-fluid"/>
